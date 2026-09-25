598cc4c961f2e322cdc727dc6d4280315d7c9c5a
gianniosgeorgios <gianniosgeorgios45@gmail.com>
2026-09-25 10:50:23 +0300

    test

--- student/solution.cpp
+++ student/solution.cpp
@@ -1,27 +1,43 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 // Student function: find the maximum value in an array in O(N) time.
-int find_max(const vector<int>& arr) {
-    if (arr.empty()) {
+int find_max(int N, int a[]) {
+    if (N <= 0) {
         return 0;
     }
 
-    int max_value = arr[0];
-    for (int i = 1; i < static_cast<int>(arr.size()); ++i) {
-        if (arr[i] > max_value) {
-            max_value = arr[i];
+    int max_value = a[0];
+    for (int i = 1; i < N; ++i) {
+        if (a[i] > max_value) {
+            max_value = a[i];
         }
     }
     return max_value;
 }
 
+// Student function: find the minimum value in an array in O(N) time.
+int find_min(int N, int a[]) {
+    if (N <= 0) {
+        return 0;
+    }
+
+    int min_value = a[0];
+    for (int i = 1; i < N; ++i) {
+        if (a[i] < min_value) {
+            min_value = a[i];
+        }
+    }
+    return min_value;
+}
+
 int main() {
-    vector<int> sample = {3, 8, 1, 9, 4, 12, 5, 7};
+    int sample[] = {3, 8, 1, 9, 4, 12, 5, 7};
+    int N = 8;
 
     cout << "Example array: 3 8 1 9 4 12 5 7\n";
-    cout << "Maximum value = " << find_max(sample) << '\n';
+    cout << "Maximum value = " << find_max(N, sample) << '\n';
+    cout << "Minimum value = " << find_min(N, sample) << '\n';
 
     return 0;
 }
