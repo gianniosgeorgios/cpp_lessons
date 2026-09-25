e17fe273a832c48528bf84996cd06e51df88c099
gianniosgeorgios <gianniosgeorgios45@gmail.com>
2026-09-25 10:56:43 +0300

    test

--- student/day_02/solution.cpp
+++ student/day_02/solution.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 // Student function: find the minimum value in an array in O(N) time.
-int find_min(const vector<int>& arr) {
-    if (arr.empty()) {
+int find_min(int N, int a[]) {
+    if (N <= 0) {
         return 0;
     }
 
-    int min_value = arr[0];
-    for (int i = 1; i < static_cast<int>(arr.size()); ++i) {
-        if (arr[i] < min_value) {
-            min_value = arr[i];
+    int min_value = a[0];
+    for (int i = 1; i < N; ++i) {
+        if (a[i] < min_value) {
+            min_value = a[i];
         }
     }
     return min_value;
 }
 
 int main() {
-    vector<int> sample = {8, 3, 12, 9, 4, 7, 1, 5};
+    int sample[] = {8, 3, 12, 9, 4, 7, 1, 5};
+    int N = 8;
 
     cout << "Example array: 8 3 12 9 4 7 1 5\n";
-    cout << "Minimum value = " << find_min(sample) << '\n';
+    cout << "Minimum value = " << find_min(N, sample) << '\n';
 
     return 0;
-}
+
